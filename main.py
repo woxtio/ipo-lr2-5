61931f82a1c152d9c51c3c0bda734a428e5cdb57
dasha <dpaseka7@gmail.com>
2024-10-02 09:25:21 +0300

    main.py

--- main.py
+++ main.py
@@ -3,5 +3,8 @@ class1 = int(input("Введите количество учащихся в пе
 class2 = int(input("Введите количество учащихся во втором классе: "))
 class3 = int(input("Введите количество учащихся в третьем классе: "))
 people = class1 + class2 + class3
-desks = math.ceil(people / 2)
+desks1 = math.ceil(class1/ 2)
+desks2 = math.ceil(class2 / 2)
+desks3 = math.ceil(class3 / 2)
+desks =  desks1 + desks2 + desks3
 print("Необходимо закупить парт:", desks)
